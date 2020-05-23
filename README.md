3b0b87dfe1e49b5c95728240f764e193ad9347a9
JDibnah <jamesdibnah@gmail.com>
2020-05-23 14:00:04 +0100

    Dynamic config location
    Changes:
     - Updated: Config dynamic location

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SANDRA_Main
 Home automation system - V6
+
+
+references:
+Had troubles installing Spacy but this solution helped:
+BLIS_ARCH="generic" pip3 install spacy
+https://github.com/explosion/spaCy/issues/4124
--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import json
+import os
+
 CONFIG = {}
 
 def setup():
-    with open('./config.json') as f:
+    with open(os.path.join(os.getcwd(), 'config.json')) as f:
         global CONFIG
         CONFIG = json.load(f)
